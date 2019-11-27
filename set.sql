3281097022891a78bec0a9936cd03ff283f021be
naufalirfan <nnaufall@gmail.com>
2019-11-28 01:30:26 +0700

    small fix & change SQL DB

--- set.sql
+++ set.sql
@@ -1,16 +1,21 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 CREATE TABLE `redirect` (
-  `id` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `url` varchar(620) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  `url` varchar(620) COLLATE utf8mb4_bin NOT NULL,
   `date` datetime NOT NULL,
   `hits` bigint(20) NOT NULL DEFAULT '0',
-  `user` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'public'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Used for the URL shortener';
+  `user` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT 'public'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='Used for the URL shortener' ROW_FORMAT=COMPACT;
+
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
+
 
 ALTER TABLE `redirect`
   ADD PRIMARY KEY (`id`);
@@ -20,4 +25,4 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `username` (`username`);
 
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
